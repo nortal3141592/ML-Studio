f30e925bccf115fe312fb6cbddf6f2701ed4bb11
nortal3141592 <palin3200jena@gmail.com>
2026-07-19 17:03:06 +0530

    Removed some unecessary comments from file_utils.py

--- app/utils/file_utils.py
+++ app/utils/file_utils.py
@@ -1,29 +1,8 @@
 from pathlib import Path
 import pandas as pd
-from enum_utils import DatasetStage
+from utils.enum_utils import DatasetStage
 
 UPLOADS_DIR = Path("uploads")
-
-# def save_raw_dataset(df: pd.DataFrame, project_id: int) -> str:
-#     project_dir = UPLOADS_DIR / f"project_{project_id}"
-#     project_dir.mkdir(parents=True, exist_ok=True)
-
-#     filename = "raw.parquet"
-#     filepath = project_dir / filename
-
-#     df.to_parquet(filepath)
-
-#     return str(filepath)
-
-# def save_cleaned_dataset(df: pd.DataFrame, project_id: int) -> str:
-#     project_dir = UPLOADS_DIR / f"project_{project_id}"
-#     project_dir.mkdir(parents=True, exist_ok=True)
-
-#     filename = "cleaned.parquet"
-#     filepath = project_dir / filename
-
-#     df.to_parquet(filepath)
-#     return str(filepath)
 
 def save_dataset(df: pd.DataFrame, project_id: int, stage: DatasetStage) -> str:
     project_dir = UPLOADS_DIR / f"project_{project_id}"
