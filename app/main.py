24beaff6a0c955c594bee5295b6403add9397407
Palin Jena <palin3200jena@gmail.com>
2026-07-28 15:21:32 +0530

    Update CORS allowed origins for production

--- app/main.py
+++ app/main.py
@@ -24,7 +24,7 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["https://ml-studio-seven.vercel.app/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
